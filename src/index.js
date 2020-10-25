8726ae5038c4aa90de6821f4863554bcbcb17e0e
Maksim Ivankou <fahrenheit9110@gmail.com>
2020-10-25 18:05:19 +0300

    init: start morse-decoder task

--- src/index.js
+++ src/index.js
@@ -37,10 +37,83 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+function translate(str) {
+  let letter = MORSE_TABLE[str];
+  let l="";
+     return letter;
+ }
+ 
 function decode(expr) {
-    // write your solution here
+  //   tmpArrayNumber = [];
+  //   tmpStringNumber = "";
+    
+  //   for (let i = 0; i < expr.length; i += 10) {
+  //     tmpStringNumber = expr.substring(i, i + 10)
+  //     oneNumber = ""
+  //     for (let j = 0; j < tmpStringNumber.length; j += 2) {
+  //       tmpOneNumber = tmpStringNumber.substring(j, j + 2)
+  //       if (tmpOneNumber == "00") {
+  //         oneNumber += ""
+  //       } else if (tmpOneNumber == "11") {
+  //         oneNumber += "-"
+  //       } else if (tmpOneNumber == "10") {
+  //         oneNumber += "."
+  //       }
+  //     }
+  
+  //     if (oneNumber == "") {
+  //       oneNumber += "**********"
+  //     }
+  //     tmpArrayNumber.push(oneNumber)
+  //     tmpStringNumber = ''
+  //   }
+  
+  
+  //   finalArray = []
+  //   for (let i = 0; i < tmpArrayNumber.length; i++) {
+  //     if (tmpArrayNumber[i] == "**********") {
+  //       finalArray.push(" ")
+  //       continue
+  //     }
+  //     for (let [key, value] of Object.entries(MORSE_TABLE)) {
+  //       if (tmpArrayNumber[i] == key) {
+  //         finalArray.push(value)
+  //       }
+  //     }
+  //   }
+  //   console.log(finalArray.join(""))
+  //   return finalArray.join("")
+  // }
+  
+    let solution = "";
+    let string = "";
+    let number = "";
+    for (let i = 0, j=1 ; i <= expr.length; i++ , j++) {
+        if(expr.charAt(i)!="0"){
+            number = expr.charAt(i) + expr.charAt(i+1);
+            if(number == "10"){
+                string += ".";
+            }
+            else if(number == "11"){
+                string += "-";
+            }
+            else if(number == "**"){
+                solution += " ";
+                i += 8;
+                j = -1;
+            }
+        }
+        i++;
+        j++;
+        if(j == 10){
+            j = 0;
+            solution += translate(string);
+            string = "";
+        }
+    }
+    return solution;
 }
-
-module.exports = {
+  
+  module.exports = {
     decode
-}
+  }
